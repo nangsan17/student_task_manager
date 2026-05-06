a6796c04ae40b1c9ecc9035e349ef9305b245a98
Nang Thet Htar San <htarsannangthet@gmail.com>
2026-05-06 14:31:22 +0800

    firebase Intalled
    changed main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
@@ -27,11 +30,11 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
+      appBar: AppBar(title: const Text('Student Task Manager')),
       body: const Center(
         child: Text(
-          'App Started Successfully 🚀',
-          style: TextStyle(fontSize: 18),
+          'Firebase Connected Successfully 🚀',
+          style: TextStyle(fontSize: 22),
         ),
       ),
     );
